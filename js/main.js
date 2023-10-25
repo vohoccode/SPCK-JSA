6dffd4944dfe4648523b1052069e5c3906ae0411
maidzui123 <maiduy190802@gmail.com>
2023-10-25 19:55:04 +0700

    upd4

--- js/main.js
+++ js/main.js
@@ -69,6 +69,30 @@ var product = [
         image: './image/Christmas Gingerbread House Arch.jpg',
         type: 'Inflatable Christmas decoration',
         color: 'Brown',
-        material: "	Polyester 190 T" 
+        material: "	Polyester 190 T"
     },
 ]
+
+var countDownDate = new Date("Dec 25, 2023 00:00:00").getTime()
+var countDown = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24))
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+    var seconds = Math.floor(distance % (1000 * 60) / 1000)
+
+    document.getElementById("days").innerHTML = days
+    document.getElementById("hours").innerHTML = hours
+    document.getElementById("minutes").innerHTML = minutes
+    document.getElementById("seconds").innerHTML = seconds
+
+    if (distance < 0) {
+        clearInterval()
+        document.getElementById("days").innerHTML = days
+        document.getElementById("hours").innerHTML = hours
+        document.getElementById("minutes").innerHTML = minutes
+        document.getElementById("seconds").innerHTML = seconds
+    }
+}, 1000)
